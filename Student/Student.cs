00f7da9a2337c01c4f139fd00d24bdc6c3a5504a
msunkist99 <msunkist99@gmail.com>
2019-03-04 15:01:05 -0600

    2019/03/04 initial code

---
+++ Student/Student.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace StudentClass
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int[] Scores { get; set; }
+
+        public Student[] students { get; set; }
+
+        public Student() { }
+
+        public Student[] GetStudentsFromTextFile(string path, string fileName)
+        {
+            string[] studentsText = File.ReadAllLines(path + "\\" + fileName);
+
+            // create an array of Student classes from thestudentdata text
+            Student[] students = new Student[studentsText.Length];
+
+            for (int i = 0; i < studentsText.Length; i++)
+            {
+                {
+                    string[] studentTextInputFields = studentsText[i].Split(" ");
+                    Student student = new Student();
+                    student.Name = studentTextInputFields[0];
+
+                    int[] scores = new int[studentTextInputFields.Length - 1];
+
+                    for (int j = 1; j < studentTextInputFields.Length; j++)
+                    {
+                        scores[j - 1] = int.Parse(studentTextInputFields[j]);
+
+                    }
+                    student.Scores = scores;
+                    students[i] = student;
+                }
+            }
+
+            return students;
+        }
+
+        public int? GetAverage()
+        {
+            int scoreSum = 0;
+
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                scoreSum += Scores[i];
+            }
+
+            return (scoreSum / Scores.Length);
+        }
+
+        public bool HasSixOrMore()
+        {
+            return (Scores.Length > 5) ? true : false;
+        }
+
+        public int? GetMaximumScore()
+        {
+            int maxScore = 0;
+
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (Scores[i] > maxScore)
+                {
+                    maxScore = Scores[i];
+                }
+            }
+
+            return maxScore;
+        }
+
+        public int? GetMinimumScore()
+        {
+            int minScore = 100;
+
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                if (Scores[i] < minScore)
+                {
+                    minScore = Scores[i];
+                }
+            }
+
+            return minScore;
+        }
+    }
+}
---
+++ StudentConsolidated/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using StudentClass;
+
+
+namespace StudentConsolidated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read from the studentdata.txt file
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+
+            Student student = new Student();
+
+            Student[] students = student.GetStudentsFromTextFile("C:\\Users\\msunk\\Documents\\CoderGirlLC101\\Lesson09 CoderGirl-StudentConsolidatedSolution\\CoderGirl-StudentConsolidated", 
+                                                                 "studentdata.txt");
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i].Name + " - Average - " + students[i].GetAverage());
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i].Name + " - Maximum - " + students[i].GetMaximumScore() + " - Minimum - " + students[i].GetMinimumScore());
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i].Name + " - 6 or more - " + students[i].HasSixOrMore());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
